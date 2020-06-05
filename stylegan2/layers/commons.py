c86aa97acc8017fbfe6486b31176d18c406ed6f5
moono <toilety@gmail.com>
2020-06-05 21:42:37 +0900

    remove numpy

--- stylegan2/layers/commons.py
+++ stylegan2/layers/commons.py
@@ -1,10 +1,10 @@
-import numpy as np
 import tensorflow as tf
 
 
 def compute_runtime_coef(weight_shape, gain, lrmul):
-    fan_in = np.prod(weight_shape[:-1])  # [kernel, kernel, fmaps_in, fmaps_out] or [in, out]
-    he_std = gain / np.sqrt(fan_in)
+    fan_in = tf.reduce_prod(weight_shape[:-1])  # [kernel, kernel, fmaps_in, fmaps_out] or [in, out]
+    fan_in = tf.cast(fan_in, dtype=tf.float32)
+    he_std = gain / tf.sqrt(fan_in)
     init_std = 1.0 / lrmul
     runtime_coef = he_std * lrmul
     return init_std, runtime_coef
