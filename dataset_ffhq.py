7972ca38c8a2bbe14eabce88d2611362c56add6a
moono <toilety@gmail.com>
2020-06-06 12:34:55 +0900

    fix bugs

--- dataset_ffhq.py
+++ dataset_ffhq.py
@@ -19,36 +19,30 @@ def parse_tfrecord_tf(record):
     return images
 
 
-def get_ffhq_dataset(tfrecord_base_dir, res, buffer_size, batch_size, epochs=None):
+def get_ffhq_dataset(tfrecord_base_dir, res, batch_size, epochs=None, buffer_size=1000):
     fn_index = int(np.log2(res))
     tfrecord_fn = os.path.join(tfrecord_base_dir, 'ffhq-r{:02d}.tfrecords'.format(fn_index))
 
-    with tf.device('/cpu:0'):
-        dataset = tf.data.TFRecordDataset(tfrecord_fn)
-        dataset = dataset.map(map_func=parse_tfrecord_tf, num_parallel_calls=8)
-        dataset = dataset.shuffle(buffer_size=buffer_size)
-        dataset = dataset.repeat(epochs)
-        dataset = dataset.batch(batch_size)
-        dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    # with tf.device('/cpu:0'):
+    dataset = tf.data.TFRecordDataset(tfrecord_fn)
+    dataset = dataset.map(map_func=parse_tfrecord_tf, num_parallel_calls=8)
+    dataset = dataset.shuffle(buffer_size=buffer_size)
+    dataset = dataset.repeat(epochs)
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
     return dataset
 
 
-def test_input_fn(tfrecord_dir):
-    n_samples = 70000
-    res = 32
+def main():
+    res = 64
     batch_size = 4
     epochs = 1
+    tfrecord_dir = './tfrecords'
 
     dataset = get_ffhq_dataset(tfrecord_dir, res, batch_size, epochs)
 
     for real_images in dataset.take(1):
         print(real_images.shape)
-    return
-
-
-def main():
-    tfrecord_dir = './tfrecords'
-    test_input_fn(tfrecord_dir)
     return
 
 
