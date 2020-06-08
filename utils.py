adb3b621409ebc6e9790897c4666cd5c8203b182
Mookyung Song <moono.song@ncsoft.com>
2020-06-08 12:39:19 +0900

    add helper

---
+++ utils.py
@@ -0,0 +1,8 @@
+def str_to_bool(value):
+    if isinstance(value, bool):
+        return value
+    if value.lower() in {'false', 'f', '0', 'no', 'n'}:
+        return False
+    elif value.lower() in {'true', 't', '1', 'yes', 'y'}:
+        return True
+    raise ValueError(f'{value} is not a valid boolean value')
