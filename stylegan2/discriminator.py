possible backpropagation fixes with @tf.function enabled, the shapes values(int) converted as NoneType, and it will cause errors for shape computing. To fix, one has to manually pass all required shapes

stylegan2/discriminator.py
@@ -18,17 +18,17 @@ class DiscriminatorBlock(tf.keras.layers.Layer):
         self.resnet_scale = 1. / tf.sqrt(2.)
 
         # conv_0
-        self.conv_0 = Conv2D(fmaps=self.n_f0, kernel=3, up=False, down=False,
+        self.conv_0 = Conv2D(in_res=res, in_fmaps=self.n_f0, fmaps=self.n_f0, kernel=3, up=False, down=False,
                              resample_kernel=None, gain=self.gain, lrmul=self.lrmul, name='conv_0')
         self.apply_bias_act_0 = BiasAct(lrmul=self.lrmul, act='lrelu', name='bias_0')
 
         # conv_1 down
-        self.conv_1 = Conv2D(fmaps=self.n_f1, kernel=3, up=False, down=True,
+        self.conv_1 = Conv2D(in_res=res, in_fmaps=self.n_f0, fmaps=self.n_f1, kernel=3, up=False, down=True,
                              resample_kernel=[1, 3, 3, 1], gain=self.gain, lrmul=self.lrmul, name='conv_1')
         self.apply_bias_act_1 = BiasAct(lrmul=self.lrmul, act='lrelu', name='bias_1')
 
         # resnet skip
-        self.conv_skip = Conv2D(fmaps=self.n_f1, kernel=1, up=False, down=True,
+        self.conv_skip = Conv2D(in_res=res, in_fmaps=self.n_f0, fmaps=self.n_f1, kernel=1, up=False, down=True,
                                 resample_kernel=[1, 3, 3, 1], gain=self.gain, lrmul=self.lrmul, name='skip')
 
     def call(self, inputs, training=None, mask=None):
@@ -61,7 +61,7 @@ class DiscriminatorLastBlock(tf.keras.layers.Layer):
         self.minibatch_std = MinibatchStd(group_size=4, num_new_features=1, name='minibatchstd')
 
         # conv_0
-        self.conv_0 = Conv2D(fmaps=self.n_f0, kernel=3, up=False, down=False,
+        self.conv_0 = Conv2D(in_res=res, in_fmaps=self.n_f0 + 1, fmaps=self.n_f0, kernel=3, up=False, down=False,
                              resample_kernel=None, gain=self.gain, lrmul=self.lrmul, name='conv_0')
         self.apply_bias_act_0 = BiasAct(lrmul=self.lrmul, act='lrelu', name='bias_0')
 
@@ -92,7 +92,7 @@ class Discriminator(tf.keras.Model):
 
         # stack discriminator blocks
         res0, n_f0 = self.r_resolutions[0], self.r_featuremaps[0]
-        self.initial_fromrgb = FromRGB(fmaps=n_f0, name='{:d}x{:d}/FromRGB'.format(res0, res0))
+        self.initial_fromrgb = FromRGB(fmaps=n_f0, res=res0, name='{:d}x{:d}/FromRGB'.format(res0, res0))
         self.blocks = list()
         for index, (res0, n_f0) in enumerate(zip(self.r_resolutions[:-1], self.r_featuremaps[:-1])):
             n_f1 = self.r_featuremaps[index + 1]

stylegan2/layers/conv.py
@@ -1,19 +1,23 @@
 import tensorflow as tf
 
 from stylegan2.layers.commons import compute_runtime_coef
-from stylegan2.layers.cuda.upfirdn_2d import upsample_conv_2d, conv_downsample_2d
+from stylegan2.layers.cuda.upfirdn_2d_v2 import upsample_conv_2d, conv_downsample_2d, compute_paddings
 
 
 class Conv2D(tf.keras.layers.Layer):
-    def __init__(self, fmaps, kernel, up, down, resample_kernel, gain, lrmul, **kwargs):
+    def __init__(self, in_res, in_fmaps, fmaps, kernel, up, down, resample_kernel, gain, lrmul, **kwargs):
         super(Conv2D, self).__init__(**kwargs)
+        self.in_res = in_res
+        self.in_fmaps = in_fmaps
         self.fmaps = fmaps
         self.kernel = kernel
         self.gain = gain
         self.lrmul = lrmul
         self.up = up
         self.down = down
-        self.resample_kernel = resample_kernel
+        # self.resample_kernel = resample_kernel
+
+        self.k, self.pad0, self.pad1 = compute_paddings(resample_kernel, self.kernel, up, down, is_conv=True)
 
     def build(self, input_shape):
         weight_shape = [self.kernel, self.kernel, input_shape[1], self.fmaps]
@@ -25,26 +29,13 @@ class Conv2D(tf.keras.layers.Layer):
 
     def call(self, inputs, training=None, mask=None):
         x = inputs
-        weight = self.runtime_coef * self.w
+        w = self.runtime_coef * self.w
 
         # actual conv
         if self.up:
-            x = upsample_conv_2d(x, weight, data_format='NCHW', k=self.resample_kernel)
+            x = upsample_conv_2d(x, self.in_res, w, self.kernel, self.kernel, self.pad0, self.pad1, self.k)
         elif self.down:
-            x = conv_downsample_2d(x, weight, data_format='NCHW', k=self.resample_kernel)
+            x = conv_downsample_2d(x, self.in_res, w, self.kernel, self.kernel, self.pad0, self.pad1, self.k)
         else:
-            x = tf.nn.conv2d(x, weight, data_format='NCHW', strides=[1, 1, 1, 1], padding='SAME')
+            x = tf.nn.conv2d(x, w, data_format='NCHW', strides=[1, 1, 1, 1], padding='SAME')
         return x
-
-    def get_config(self):
-        config = super(Conv2D, self).get_config()
-        config.update({
-            'fmaps': self.fmaps,
-            'kernel': self.kernel,
-            'gain': self.gain,
-            'lrmul': self.lrmul,
-            'up': self.up,
-            'down': self.down,
-            'resample_kernel': self.resample_kernel,
-        })
-        return config

stylegan2/layers/from_rgb.py
@@ -5,11 +5,12 @@ from stylegan2.layers.bias_act import BiasAct
 
 
 class FromRGB(tf.keras.layers.Layer):
-    def __init__(self, fmaps, **kwargs):
+    def __init__(self, fmaps, res, **kwargs):
         super(FromRGB, self).__init__(**kwargs)
         self.fmaps = fmaps
+        self.res = res
 
-        self.conv = Conv2D(fmaps=self.fmaps, kernel=1, up=False, down=False,
+        self.conv = Conv2D(in_res=res, in_fmaps=3, fmaps=self.fmaps, kernel=1, up=False, down=False,
                            resample_kernel=None, gain=1.0, lrmul=1.0, name='conv')
         self.apply_bias_act = BiasAct(lrmul=1.0, act='lrelu', name='bias')
 

stylegan2/layers/modulated_conv2d.py
@@ -3,13 +3,16 @@ import tensorflow as tf
 from stylegan2.layers.commons import compute_runtime_coef
 from stylegan2.layers.dense import Dense
 from stylegan2.layers.bias_act import BiasAct
-from stylegan2.layers.cuda.upfirdn_2d import upsample_conv_2d, conv_downsample_2d
+from stylegan2.layers.cuda.upfirdn_2d_v2 import upsample_conv_2d, conv_downsample_2d, compute_paddings
 
 
 class ModulatedConv2D(tf.keras.layers.Layer):
-    def __init__(self, in_fmaps, fmaps, kernel, up, down, demodulate, resample_kernel, gain, lrmul, fused_modconv, **kwargs):
+    def __init__(self, in_res, in_fmaps, fmaps, kernel, up, down, demodulate, resample_kernel, gain, lrmul,
+                 fused_modconv, **kwargs):
         super(ModulatedConv2D, self).__init__(**kwargs)
         assert not (up and down)
+
+        self.in_res = in_res
         self.in_fmaps = in_fmaps
         self.fmaps = fmaps
         self.kernel = kernel
@@ -17,9 +20,11 @@ class ModulatedConv2D(tf.keras.layers.Layer):
         self.up = up
         self.down = down
         self.fused_modconv = fused_modconv
-        self.resample_kernel = resample_kernel
         self.gain = gain
         self.lrmul = lrmul
+        # self.resample_kernel = resample_kernel
+
+        self.k, self.pad0, self.pad1 = compute_paddings(resample_kernel, self.kernel, up, down, is_conv=True)
 
         # self.factor = 2
         self.mod_dense = Dense(self.in_fmaps, gain=1.0, lrmul=1.0, name='mod_dense')
@@ -63,9 +68,9 @@ class ModulatedConv2D(tf.keras.layers.Layer):
 
         # Convolution with optional up/downsampling.
         if self.up:
-            x = upsample_conv_2d(x, w, data_format='NCHW', k=self.resample_kernel)
+            x = upsample_conv_2d(x, self.in_res, w, self.kernel, self.kernel, self.pad0, self.pad1, self.k)
         elif self.down:
-            x = conv_downsample_2d(x, w, data_format='NCHW', k=self.resample_kernel)
+            x = conv_downsample_2d(x, self.in_res, w, self.kernel, self.kernel, self.pad0, self.pad1, self.k)
         else:
             x = tf.nn.conv2d(x, w, data_format='NCHW', strides=[1, 1, 1, 1], padding='SAME')
 

stylegan2/layers/synthesis_block.py
@@ -4,7 +4,7 @@ from stylegan2.layers.modulated_conv2d import ModulatedConv2D
 from stylegan2.layers.bias_act import BiasAct
 from stylegan2.layers.noise import Noise
 from stylegan2.layers.to_rgb import ToRGB
-from stylegan2.layers.cuda.upfirdn_2d import upsample_2d
+from stylegan2.layers.cuda.upfirdn_2d_v2 import upsample_2d, compute_paddings
 
 
 class SynthesisConstBlock(tf.keras.layers.Layer):
@@ -17,7 +17,7 @@ class SynthesisConstBlock(tf.keras.layers.Layer):
         self.lrmul = 1.0
 
         # conv block
-        self.conv = ModulatedConv2D(in_fmaps=self.fmaps, fmaps=self.fmaps, kernel=3, up=False, down=False,
+        self.conv = ModulatedConv2D(in_res=res, in_fmaps=self.fmaps, fmaps=self.fmaps, kernel=3, up=False, down=False,
                                     demodulate=True, resample_kernel=[1, 3, 3, 1], gain=self.gain, lrmul=self.lrmul,
                                     fused_modconv=True, name='conv')
         self.apply_noise = Noise(name='noise')
@@ -63,14 +63,14 @@ class SynthesisBlock(tf.keras.layers.Layer):
         self.lrmul = 1.0
 
         # conv0 up
-        self.conv_0 = ModulatedConv2D(in_fmaps=self.in_ch, fmaps=self.fmaps, kernel=3, up=True, down=False,
+        self.conv_0 = ModulatedConv2D(in_res=res//2, in_fmaps=self.in_ch, fmaps=self.fmaps, kernel=3, up=True, down=False,
                                       demodulate=True, resample_kernel=[1, 3, 3, 1], gain=self.gain, lrmul=self.lrmul,
                                       fused_modconv=True, name='conv_0')
         self.apply_noise_0 = Noise(name='noise_0')
         self.apply_bias_act_0 = BiasAct(lrmul=self.lrmul, act='lrelu', name='bias_0')
 
         # conv block
-        self.conv_1 = ModulatedConv2D(in_fmaps=self.fmaps, fmaps=self.fmaps, kernel=3, up=False, down=False,
+        self.conv_1 = ModulatedConv2D(in_res=res, in_fmaps=self.fmaps, fmaps=self.fmaps, kernel=3, up=False, down=False,
                                       demodulate=True, resample_kernel=[1, 3, 3, 1], gain=self.gain, lrmul=self.lrmul,
                                       fused_modconv=True, name='conv_1')
         self.apply_noise_1 = Noise(name='noise_1')
@@ -107,12 +107,14 @@ class Synthesis(tf.keras.layers.Layer):
         super(Synthesis, self).__init__(name=name, **kwargs)
         self.resolutions = resolutions
         self.featuremaps = featuremaps
-        self.resample_kernel = [1, 3, 3, 1]
+        # self.resample_kernel = [1, 3, 3, 1]
+
+        self.k, self.pad0, self.pad1 = compute_paddings([1, 3, 3, 1], None, up=True, down=False, is_conv=False)
 
         # initial layer
         res, n_f = resolutions[0], featuremaps[0]
         self.initial_block = SynthesisConstBlock(fmaps=n_f, res=res, name='{:d}x{:d}/const'.format(res, res))
-        self.initial_torgb = ToRGB(in_ch=n_f, name='{:d}x{:d}/ToRGB'.format(res, res))
+        self.initial_torgb = ToRGB(in_ch=n_f, res=res, name='{:d}x{:d}/ToRGB'.format(res, res))
 
         # stack generator block with lerp block
         prev_n_f = n_f
@@ -121,7 +123,7 @@ class Synthesis(tf.keras.layers.Layer):
         for res, n_f in zip(self.resolutions[1:], self.featuremaps[1:]):
             self.blocks.append(SynthesisBlock(in_ch=prev_n_f, fmaps=n_f, res=res,
                                               name='{:d}x{:d}/block'.format(res, res)))
-            self.torgbs.append(ToRGB(in_ch=n_f, name='{:d}x{:d}/ToRGB'.format(res, res)))
+            self.torgbs.append(ToRGB(in_ch=n_f, res=res * 2, name='{:d}x{:d}/ToRGB'.format(res, res)))
             prev_n_f = n_f
 
     def call(self, inputs, training=None, mask=None):
@@ -138,8 +140,9 @@ class Synthesis(tf.keras.layers.Layer):
             w1 = w_broadcasted[:, layer_index + 1]
             w2 = w_broadcasted[:, layer_index + 2]
 
+            y_res = block.res // 2
             x = block([x, w0, w1])
-            y = upsample_2d(y, self.resample_kernel, factor=2, gain=1.0)
+            y = upsample_2d(y, y_res, self.pad0, self.pad1, self.k)
             y = y + torgb([x, w2])
 
             layer_index += 2

stylegan2/layers/to_rgb.py
@@ -5,10 +5,12 @@ from stylegan2.layers.bias_act import BiasAct
 
 
 class ToRGB(tf.keras.layers.Layer):
-    def __init__(self, in_ch, **kwargs):
+    def __init__(self, in_ch, res, **kwargs):
         super(ToRGB, self).__init__(**kwargs)
         self.in_ch = in_ch
-        self.conv = ModulatedConv2D(in_fmaps=in_ch, fmaps=3, kernel=1, up=False, down=False, demodulate=False,
+        self.res = res
+
+        self.conv = ModulatedConv2D(in_res=res, in_fmaps=in_ch, fmaps=3, kernel=1, up=False, down=False, demodulate=False,
                                     resample_kernel=None, gain=1.0, lrmul=1.0, fused_modconv=True, name='conv')
         self.apply_bias = BiasAct(lrmul=1.0, act='linear', name='bias')
 

stylegan2/losses.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 def d_logistic(generator, discriminator, real_images, batch_size, z_dim, labels_dim):
     z = tf.random.normal(shape=[batch_size, z_dim], dtype=tf.float32)
-    labels = tf.ones(shape=[batch_size, labels_dim], dtype=tf.float32)
+    labels = tf.random.normal(shape=[batch_size, labels_dim], dtype=tf.float32)
 
     # forward pass
     fake_images = generator([z, labels], training=True)
@@ -18,7 +18,7 @@ def d_logistic(generator, discriminator, real_images, batch_size, z_dim, labels_
 
 def d_logistic_r1(generator, discriminator, real_images, batch_size, z_dim, labels_dim):
     z = tf.random.normal(shape=[batch_size, z_dim], dtype=tf.float32)
-    labels = tf.ones(shape=[batch_size, labels_dim], dtype=tf.float32)
+    labels = tf.random.normal(shape=[batch_size, labels_dim], dtype=tf.float32)
 
     # forward pass
     fake_images = generator([z, labels], training=True)
@@ -42,7 +42,7 @@ def d_logistic_r1(generator, discriminator, real_images, batch_size, z_dim, labe
 
 def g_logistic_non_saturating(generator, discriminator, batch_size, z_dim, labels_dim):
     z = tf.random.normal(shape=[batch_size, z_dim], dtype=tf.float32)
-    labels = tf.ones(shape=[batch_size, labels_dim], dtype=tf.float32)
+    labels = tf.random.normal(shape=[batch_size, labels_dim], dtype=tf.float32)
 
     # forward pass
     fake_images = generator([z, labels], training=True)
@@ -56,11 +56,11 @@ def g_logistic_non_saturating(generator, discriminator, batch_size, z_dim, label
 def g_logistic_ns_pathreg(generator, discriminator, batch_size, z_dim, labels_dim,
                           pl_mean, pl_minibatch_shrink=2, pl_decay=0.01):
     z = tf.random.normal(shape=[batch_size, z_dim], dtype=tf.float32)
-    labels = tf.ones(shape=[batch_size, labels_dim], dtype=tf.float32)
+    labels = tf.random.normal(shape=[batch_size, labels_dim], dtype=tf.float32)
 
     pl_minibatch = tf.math.floordiv(batch_size, pl_minibatch_shrink)
     pl_z = tf.random.normal(shape=[pl_minibatch, z_dim], dtype=tf.float32)
-    pl_labels = tf.ones(shape=[pl_minibatch, labels_dim], dtype=tf.float32)
+    pl_labels = tf.random.normal(shape=[pl_minibatch, labels_dim], dtype=tf.float32)
 
     # forward pass
     fake_images, w_broadcasted = generator([z, labels], ret_w_broadcasted=True, training=True)
@@ -85,23 +85,3 @@ def g_logistic_ns_pathreg(generator, discriminator, batch_size, z_dim, labels_di
     # Calculate (|J*y|-a)^2.
     pl_penalty = tf.square(pl_lengths - pl_mean)
     return g_loss, pl_penalty
-
-
-# def pl_reg(fake_images, w_broadcasted, pl_mean, pl_decay=0.01):
-#     h, w = fake_images.shape[2], fake_images.shape[3]
-#
-#     # Compute |J*y|.
-#     with tf.GradientTape() as pl_tape:
-#         pl_tape.watch(w_broadcasted)
-#         pl_noise = tf.random.normal(tf.shape(fake_images), mean=0.0, stddev=1.0, dtype=tf.float32) / np.sqrt(h * w)
-#
-#     pl_grads = pl_tape.gradient(tf.reduce_sum(fake_images * pl_noise), w_broadcasted)
-#     pl_lengths = tf.math.sqrt(tf.reduce_mean(tf.reduce_sum(tf.math.square(pl_grads), axis=2), axis=1))
-#
-#     # Track exponential moving average of |J*y|.
-#     pl_mean_val = pl_mean + pl_decay * (tf.reduce_mean(pl_lengths) - pl_mean)
-#     pl_mean.assign(pl_mean_val)
-#
-#     # Calculate (|J*y|-a)^2.
-#     pl_penalty = tf.square(pl_lengths - pl_mean)
-#     return pl_penalty