364ed15743311c4cee53881499e5656cab42cfaf
Mookyung Song <toilety@gmail.com>
2020-10-13 15:11:52 +0900

    split files, add saving official generator and discriminator too

---
+++ copy_official_weights.py
@@ -0,0 +1,241 @@
+import os
+import tensorflow as tf
+
+from load_models import load_generator, load_discriminator
+
+
+def handle_mapping(w_name, is_g_clone):
+    def extract_info(name):
+        splitted = name.split('/')
+        index = splitted.index('g_mapping')
+        indicator = splitted[index + 1]
+        val = indicator.split('_')[-1]
+        return val
+
+    level = extract_info(w_name)
+    o_prefix = f'G_mapping_1' if is_g_clone else f'G_mapping'
+    if 'w' in w_name:
+        official_var_name = f'{o_prefix}/Dense{level}/weight'
+    else:
+        official_var_name = f'{o_prefix}/Dense{level}/bias'
+    return official_var_name
+
+
+def to_rgb_layer(name, r, o_prefix):
+    if 'conv/w:0' in name:
+        o_name = f'{o_prefix}/{r}x{r}/ToRGB/weight'
+    elif 'mod_dense/w:0' in name:
+        o_name = f'{o_prefix}/{r}x{r}/ToRGB/mod_weight'
+    elif 'mod_bias/b:0' in name:
+        o_name = f'{o_prefix}/{r}x{r}/ToRGB/mod_bias'
+    else:   # if 'bias/b:0' in name:
+        o_name = f'{o_prefix}/{r}x{r}/ToRGB/bias'
+    return o_name
+
+
+def handle_block_layer(name, r, o_prefix):
+    if 'conv_0/w:0' in name:
+        o_name = f'{o_prefix}/{r}x{r}/Conv0_up/weight'
+    elif 'conv_0/mod_dense/w:0' in name:
+        o_name = f'{o_prefix}/{r}x{r}/Conv0_up/mod_weight'
+    elif 'conv_0/mod_bias/b:0' in name:
+        o_name = f'{o_prefix}/{r}x{r}/Conv0_up/mod_bias'
+    elif 'noise_0/w:0' in name:
+        o_name = f'{o_prefix}/{r}x{r}/Conv0_up/noise_strength'
+    elif 'bias_0/b:0' in name:
+        o_name = f'{o_prefix}/{r}x{r}/Conv0_up/bias'
+    elif 'conv_1/w:0' in name:
+        o_name = f'{o_prefix}/{r}x{r}/Conv1/weight'
+    elif 'conv_1/mod_dense/w:0' in name:
+        o_name = f'{o_prefix}/{r}x{r}/Conv1/mod_weight'
+    elif 'conv_1/mod_bias/b:0' in name:
+        o_name = f'{o_prefix}/{r}x{r}/Conv1/mod_bias'
+    elif 'noise_1/w:0' in name:
+        o_name = f'{o_prefix}/{r}x{r}/Conv1/noise_strength'
+    else:   # if 'bias_1/b:0' in name:
+        o_name = f'{o_prefix}/{r}x{r}/Conv1/bias'
+    return o_name
+
+
+def handle_const_layer(name, o_prefix):
+    if 'const:0' in name:
+        o_name = f'{o_prefix}/4x4/Const/const'
+    elif 'conv/w:0' in name:
+        o_name = f'{o_prefix}/4x4/Conv/weight'
+    elif 'mod_dense/w:0' in name:
+        o_name = f'{o_prefix}/4x4/Conv/mod_weight'
+    elif 'mod_bias/b:0' in name:
+        o_name = f'{o_prefix}/4x4/Conv/mod_bias'
+    elif 'noise/w:0' in name:
+        o_name = f'{o_prefix}/4x4/Conv/noise_strength'
+    else:
+        o_name = f'{o_prefix}/4x4/Conv/bias'
+    return o_name
+    
+    
+def handle_synthesis(w_name, is_g_clone):
+    def extract_info(name):
+        splitted = name.split('/')
+        index = splitted.index('g_synthesis')
+        indicator1 = splitted[index + 1]
+        indicator2 = splitted[index + 2]
+        r = indicator1.split('x')[1]
+        d = indicator2
+        return r, d
+
+    res, divider = extract_info(w_name)
+    o_prefix = f'G_synthesis_1' if is_g_clone else f'G_synthesis'
+    if divider == 'ToRGB':
+        official_var_name = to_rgb_layer(w_name, res, o_prefix)
+    elif divider == 'block':
+        official_var_name = handle_block_layer(w_name, res, o_prefix)
+    else:   # const
+        official_var_name = handle_const_layer(w_name, o_prefix)
+    return official_var_name
+
+
+def handle_discriminator_layer(w_name):
+    def extract_info(name):
+        splitted = name.split('/')
+        resolution = splitted[1]
+        resolution = resolution.split('x')[0]
+        return resolution
+
+    res = extract_info(w_name)
+    if 'last_dense' in w_name:
+        o_name = 'D/Output/weight'
+    elif 'last_bias' in w_name:
+        o_name = 'D/Output/bias'
+    elif 'FromRGB' in w_name:
+        o_name = 'D/1024x1024/FromRGB/weight' if 'conv_' in w_name else 'D/1024x1024/FromRGB/bias'
+    elif 'skip' in w_name:
+        o_name = f'D/{res}x{res}/Skip/weight'
+    elif 'dense_' in w_name:
+        o_name = f'D/4x4/Dense0/weight'
+    elif 'conv_' in w_name:
+        if res == '4':
+            o_name = f'D/{res}x{res}/Conv/weight'
+        else:
+            o_name = f'D/{res}x{res}/Conv0/weight' if '_0' in w_name else f'D/{res}x{res}/Conv1_down/weight'
+    elif 'bias_' in w_name:
+        if res == '4':
+            o_name = f'D/{res}x{res}/Conv/bias' if '_0' in w_name else f'D/{res}x{res}/Dense0/bias'
+        else:
+            o_name = f'D/{res}x{res}/Conv0/bias' if '_0' in w_name else f'D/{res}x{res}/Conv1_down/bias'
+    else:
+        raise ValueError('Something went wrong!!')
+    return o_name
+
+
+def variable_name_mapper_g(g, is_g_clone):
+    name_mapper = dict()
+    for w in g.weights:
+        w_name, w_shape = w.name, w.shape
+
+        # mapping layer
+        if 'g_mapping' in w_name:
+            official_var_name = handle_mapping(w_name, is_g_clone)
+        elif 'g_synthesis' in w_name:
+            official_var_name = handle_synthesis(w_name, is_g_clone)
+        else:
+            # w_avg
+            official_var_name = 'Gs/dlatent_avg' if is_g_clone else 'G/dlatent_avg'
+
+        name_mapper[official_var_name] = w
+    return name_mapper
+
+
+def variable_name_mapper_d(d):
+    name_mapper = dict()
+    for w in d.weights:
+        w_name, w_shape = w.name, w.shape
+
+        official_var_name = handle_discriminator_layer(w_name)
+        name_mapper[official_var_name] = w
+    return name_mapper
+
+
+def check_shape(name_mapper, official_vars):
+    for official_name, v in name_mapper.items():
+        official_shape = [s for n, s in official_vars if n == official_name][0]
+
+        if official_shape == v.shape:
+            print('{}: shape matches'.format(official_name))
+        else:
+            # print(f'Official: {official_name} -> {official_shape}')
+            # print(f'Current: {v.name} -> {v.shape}')
+            raise ValueError('{}: wrong shape'.format(official_name))
+    return
+
+
+def convert_official_generator_weights(ckpt_dir, is_g_clone, use_custom_cuda):
+    generator = load_generator(g_params=None, is_g_clone=is_g_clone, ckpt_dir=None, custom_cuda=use_custom_cuda)
+
+    # restore official ones to current implementation
+    official_checkpoint = tf.train.latest_checkpoint('./official-pretrained')
+    official_vars = tf.train.list_variables(official_checkpoint)
+
+    # get name mapper
+    name_mapper = variable_name_mapper_g(generator, is_g_clone=is_g_clone)
+    for name_g, tvar in name_mapper.items():
+        print(f'{name_g}: {tvar.name}')
+
+    # check shape
+    check_shape(name_mapper, official_vars)
+
+    # restore
+    tf.compat.v1.train.init_from_checkpoint(official_checkpoint, assignment_map=name_mapper)
+
+    # save
+    if is_g_clone:
+        ckpt = tf.train.Checkpoint(g_clone=generator)
+    else:
+        ckpt = tf.train.Checkpoint(generator=generator)
+    out_dir = os.path.join(ckpt_dir, 'g_clone' if is_g_clone else 'generator')
+    manager = tf.train.CheckpointManager(ckpt, out_dir, max_to_keep=1)
+    manager.save(checkpoint_number=0)
+    return
+
+
+def convert_official_discriminator_weights(ckpt_dir, use_custom_cuda):
+    discriminator = load_discriminator(d_params=None, ckpt_dir=None, custom_cuda=use_custom_cuda)
+
+    # restore official ones
+    official_checkpoint = tf.train.latest_checkpoint('./official-pretrained')
+    official_vars = tf.train.list_variables(official_checkpoint)
+
+    # get name mapper
+    name_mapper = variable_name_mapper_d(discriminator)
+    for name_d, tvar in name_mapper.items():
+        print(f'{name_d}: {tvar.name}')
+
+    # check shape
+    check_shape(name_mapper, official_vars)
+
+    # restore
+    tf.compat.v1.train.init_from_checkpoint(official_checkpoint, assignment_map=name_mapper)
+
+    # save
+    ckpt = tf.train.Checkpoint(discriminator=discriminator)
+    out_dir = os.path.join(ckpt_dir, 'discriminator')
+    manager = tf.train.CheckpointManager(ckpt, out_dir, max_to_keep=1)
+    manager.save(checkpoint_number=0)
+    return
+
+
+def main():
+    from tf_utils import allow_memory_growth
+
+    allow_memory_growth()
+
+    ckpt_dir_base = './official-converted'
+    for use_custom_cuda in [True, False]:
+        ckpt_dir = os.path.join(ckpt_dir_base, 'cuda') if use_custom_cuda else os.path.join(ckpt_dir_base, 'ref')
+        convert_official_discriminator_weights(ckpt_dir, use_custom_cuda)
+        for is_g_clone in [True, False]:
+            convert_official_generator_weights(ckpt_dir, is_g_clone, use_custom_cuda)
+    return
+
+
+if __name__ == '__main__':
+    main()
--- inference_from_official_weights.py
+++ inference_from_official_weights.py
@@ -1,30 +1,3 @@
-# ##########################################################################################################
-# To extract official weight from official repo(https://github.com/NVlabs/stylegan2), use the following code
-# ##########################################################################################################
-# ##########################################################################################################
-# import tensorflow as tf
-# import pretrained_networks
-# 
-# 
-# network_pkl = './official_pretrained/stylegan2-ffhq-config-f.pkl'
-# print('Loading networks from "%s"...' % network_pkl)
-# _G, _D, Gs = pretrained_networks.load_networks(network_pkl)
-# print()
-# 
-# # Print network details.
-# print('===_G===')
-# _G.print_layers()
-# print('===Gs===')
-# Gs.print_layers()
-# 
-# t_var = tf.trainable_variables()
-# pprint.pprint(t_var)
-# 
-# saver = tf.train.Saver()
-# 
-# save_path = saver.save(tf.get_default_session(), "./model.ckpt")
-# ##########################################################################################################
-
 import os
 import numpy as np
 import tensorflow as tf
@@ -32,152 +5,10 @@ import tensorflow as tf
 from PIL import Image
 from stylegan2.utils import postprocess_images
 from load_models import load_generator
-from tf_utils import allow_memory_growth
+from copy_official_weights import convert_official_discriminator_weights, convert_official_generator_weights
 
 
-def handle_mapping(w_name):
-    def extract_info(name):
-        splitted = name.split('/')
-        index = splitted.index('g_mapping')
-        indicator = splitted[index + 1]
-        val = indicator.split('_')[-1]
-        return val
-
-    level = extract_info(w_name)
-    if 'w' in w_name:
-        official_var_name = 'G_mapping_1/Dense{}/weight'.format(level)
-    else:
-        official_var_name = 'G_mapping_1/Dense{}/bias'.format(level)
-    return official_var_name
-
-
-def handle_synthesis(w_name):
-    def extract_info(name):
-        splitted = name.split('/')
-        index = splitted.index('g_synthesis')
-        indicator1 = splitted[index + 1]
-        indicator2 = splitted[index + 2]
-        r = indicator1.split('x')[1]
-        d = indicator2
-        return r, d
-
-    def to_rgb_layer(name, r):
-        if 'conv/w:0' in name:
-            o_name = 'G_synthesis_1/{}x{}/ToRGB/weight'.format(r, r)
-        elif 'mod_dense/w:0' in name:
-            o_name = 'G_synthesis_1/{}x{}/ToRGB/mod_weight'.format(r, r)
-        elif 'mod_bias/b:0' in name:
-            o_name = 'G_synthesis_1/{}x{}/ToRGB/mod_bias'.format(r, r)
-        else:   # if 'bias/b:0' in name:
-            o_name = 'G_synthesis_1/{}x{}/ToRGB/bias'.format(r, r)
-        return o_name
-
-    def handle_block_layer(name, r):
-        if 'conv_0/w:0' in name:
-            o_name = 'G_synthesis_1/{}x{}/Conv0_up/weight'.format(r, r)
-        elif 'conv_0/mod_dense/w:0' in name:
-            o_name = 'G_synthesis_1/{}x{}/Conv0_up/mod_weight'.format(r, r)
-        elif 'conv_0/mod_bias/b:0' in name:
-            o_name = 'G_synthesis_1/{}x{}/Conv0_up/mod_bias'.format(r, r)
-        elif 'noise_0/w:0' in name:
-            o_name = 'G_synthesis_1/{}x{}/Conv0_up/noise_strength'.format(r, r)
-        elif 'bias_0/b:0' in name:
-            o_name = 'G_synthesis_1/{}x{}/Conv0_up/bias'.format(r, r)
-        elif 'conv_1/w:0' in name:
-            o_name = 'G_synthesis_1/{}x{}/Conv1/weight'.format(r, r)
-        elif 'conv_1/mod_dense/w:0' in name:
-            o_name = 'G_synthesis_1/{}x{}/Conv1/mod_weight'.format(r, r)
-        elif 'conv_1/mod_bias/b:0' in name:
-            o_name = 'G_synthesis_1/{}x{}/Conv1/mod_bias'.format(r, r)
-        elif 'noise_1/w:0' in name:
-            o_name = 'G_synthesis_1/{}x{}/Conv1/noise_strength'.format(r, r)
-        else:   # if 'bias_1/b:0' in name:
-            o_name = 'G_synthesis_1/{}x{}/Conv1/bias'.format(r, r)
-        return o_name
-
-    def handle_const_layer(name):
-        if 'const:0' in name:
-            o_name = 'G_synthesis_1/4x4/Const/const'
-        elif 'conv/w:0' in name:
-            o_name = 'G_synthesis_1/4x4/Conv/weight'
-        elif 'mod_dense/w:0' in name:
-            o_name = 'G_synthesis_1/4x4/Conv/mod_weight'
-        elif 'mod_bias/b:0' in name:
-            o_name = 'G_synthesis_1/4x4/Conv/mod_bias'
-        elif 'noise/w:0' in name:
-            o_name = 'G_synthesis_1/4x4/Conv/noise_strength'
-        else:
-            o_name = 'G_synthesis_1/4x4/Conv/bias'
-        return o_name
-
-    res, divider = extract_info(w_name)
-    if divider == 'ToRGB':
-        official_var_name = to_rgb_layer(w_name, res)
-    elif divider == 'block':
-        official_var_name = handle_block_layer(w_name, res)
-    else:   # const
-        official_var_name = handle_const_layer(w_name)
-    return official_var_name
-
-
-def variable_name_mapper(g):
-    name_mapper = dict()
-    for w in g.weights:
-        w_name, w_shape = w.name, w.shape
-
-        # mapping layer
-        if 'g_mapping' in w_name:
-            official_var_name = handle_mapping(w_name)
-        elif 'g_synthesis' in w_name:
-            official_var_name = handle_synthesis(w_name)
-        else:
-            official_var_name = 'Gs/dlatent_avg'
-            pass    # w_avg
-
-        name_mapper[official_var_name] = w
-    return name_mapper
-
-
-def check_shape(name_mapper, official_vars):
-    for official_name, v in name_mapper.items():
-        official_shape = [s for n, s in official_vars if n == official_name][0]
-
-        if official_shape == v.shape:
-            print('{}: shape matches'.format(official_name))
-        else:
-            # print(f'Official: {official_name} -> {official_shape}')
-            # print(f'Current: {v.name} -> {v.shape}')
-            raise ValueError('{}: wrong shape'.format(official_name))
-    return
-
-
-def convert_official_weights(ckpt_dir, use_custom_cuda):
-    g_clone = load_generator(g_params=None, is_g_clone=True, ckpt_dir=None, custom_cuda=use_custom_cuda)
-
-    # restore official ones to current implementation
-    official_checkpoint = tf.train.latest_checkpoint('./official-pretrained')
-    official_vars = tf.train.list_variables(official_checkpoint)
-    # print(official_vars)
-
-    # get name mapper
-    name_mapper = variable_name_mapper(g_clone)
-    for name1, tvar in name_mapper.items():
-        print(f'{name1}: {tvar.name}')
-
-    # check shape
-    check_shape(name_mapper, official_vars)
-
-    # restore
-    tf.compat.v1.train.init_from_checkpoint(official_checkpoint, assignment_map=name_mapper)
-
-    # save
-    ckpt = tf.train.Checkpoint(g_clone=g_clone)
-    manager = tf.train.CheckpointManager(ckpt, ckpt_dir, max_to_keep=1)
-    manager.save(checkpoint_number=0)
-    return
-
-
-def test_generator(ckpt_dir, use_custom_cuda):
+def test_generator(ckpt_dir, use_custom_cuda, out_fn):
     g_clone = load_generator(g_params=None, is_g_clone=True, ckpt_dir=ckpt_dir, custom_cuda=use_custom_cuda)
 
     # test
@@ -191,59 +22,51 @@ def test_generator(ckpt_dir, use_custom_cuda):
     image_out = postprocess_images(image_out)
     image_out = image_out.numpy()
 
-    out_fn = f'seed{seed}-restored-cuda.png' if use_custom_cuda else f'seed{seed}-restored-ref.png'
+    out_fn = f'seed{seed}-{out_fn}'
     Image.fromarray(image_out[0], 'RGB').save(out_fn)
     return
 
 
-def inference_from_each_other(ckpt_dir_base, ckpt_from='cuda', inference_from='ref'):
-    assert ckpt_from != inference_from
+def saving(ckpt_dir_base):
+    for use_custom_cuda in [True, False]:
+        ckpt_dir = os.path.join(ckpt_dir_base, 'cuda') if use_custom_cuda else os.path.join(ckpt_dir_base, 'ref')
+        convert_official_discriminator_weights(ckpt_dir, use_custom_cuda)
+        for is_g_clone in [True, False]:
+            convert_official_generator_weights(ckpt_dir, is_g_clone, use_custom_cuda)
+    return
 
-    # set proper path and variables
-    use_custom_cuda = True if inference_from == 'cuda' else False
-    ckpt_dir = os.path.join(ckpt_dir_base, 'cuda') if ckpt_from == 'cuda' else os.path.join(ckpt_dir_base, 'ref')
 
-    # create generator
-    g_clone = load_generator(g_params=None, is_g_clone=True, ckpt_dir=ckpt_dir, custom_cuda=use_custom_cuda)
+def inference(ckpt_dir_base):
+    ckpt_dir_cuda = os.path.join(ckpt_dir_base, 'cuda', 'g_clone')
+    ckpt_dir_ref = os.path.join(ckpt_dir_base, 'ref', 'g_clone')
 
-    # test
-    seed = 6600
-    rnd = np.random.RandomState(seed)
-    latents = rnd.randn(1, g_clone.z_dim)
-    labels = rnd.randn(1, g_clone.labels_dim)
-    latents = latents.astype(np.float32)
-    labels = labels.astype(np.float32)
-    image_out = g_clone([latents, labels], training=False, truncation_psi=0.5)
-    image_out = postprocess_images(image_out)
-    image_out = image_out.numpy()
+    # 1. inference cuda saved weight from cuda model
+    test_generator(ckpt_dir_cuda, use_custom_cuda=True, out_fn='from-cuda-to-cuda.png')
 
-    out_fn = f'seed{seed}-restored-from-{ckpt_from}-to-{inference_from}.png'
-    Image.fromarray(image_out[0], 'RGB').save(out_fn)
+    # 2. inference cuda saved weight from ref model
+    test_generator(ckpt_dir_cuda, use_custom_cuda=False, out_fn='from-cuda-to-ref.png')
+
+    # 3. inference ref saved weight from ref model
+    test_generator(ckpt_dir_ref, use_custom_cuda=False, out_fn='from-ref-to-ref.png')
+
+    # 4. inference ref saved weight from cuda model
+    test_generator(ckpt_dir_cuda, use_custom_cuda=True, out_fn='from-ref-to-cuda.png')
     return
 
 
 def main():
+    from tf_utils import allow_memory_growth
+
     allow_memory_growth()
 
+    # common variables
     ckpt_dir_base = './official-converted'
 
-    # phase 1-1: save official weights as cuda version
-    use_custom_cuda = True
-    ckpt_dir = os.path.join(ckpt_dir_base, 'cuda') if use_custom_cuda else os.path.join(ckpt_dir_base, 'ref')
-    convert_official_weights(ckpt_dir, use_custom_cuda)
-    test_generator(ckpt_dir, use_custom_cuda)
+    # saving phase
+    saving(ckpt_dir_base)
 
-    # phase 1-2: save official weights as ref version
-    use_custom_cuda = False
-    ckpt_dir = os.path.join(ckpt_dir_base, 'cuda') if use_custom_cuda else os.path.join(ckpt_dir_base, 'ref')
-    convert_official_weights(ckpt_dir, use_custom_cuda)
-    test_generator(ckpt_dir, use_custom_cuda)
-
-    # phase 2-1: inference cuda saved weight from ref model
-    inference_from_each_other(ckpt_dir_base, ckpt_from='cuda', inference_from='ref')
-
-    # phase 2-1: inference ref saved weight from cuda model
-    inference_from_each_other(ckpt_dir_base, ckpt_from='ref', inference_from='cuda')
+    # inference phase
+    inference(ckpt_dir_base)
     return
 
 
